ff0c3665e37235b09e418dff5db6d5a1524342ab
Agustin <rojasagustin90@gmail.com>
2016-10-01 13:09:25 -0300

    rules

--- forward-chaining/index.js
+++ forward-chaining/index.js
@@ -1,5 +1,70 @@
+"use strict";
+var fs = require('fs');
+
+function Rule() {
+	
+	this.predicate = "";
+}
+
+function Conclusion() {
+	this.core = "";
+}
+
+function RuleManager() {
+	
+	this.rules = [];
+	this.conclusion = null;
+
+	this.setRules = function(rules) {
+		this.rules = rules;
+	}
+  	this.getRules = function () { 
+  		return this.rules;
+  	}
+
+	this.evaluateRule = function(rule) {
+		console.log(rule)
+	}
+
+	this.fire = function() {
+		this.rules.foreach(function(rule) {
+			console.log(rule);
+		})
+	}
+}
+
+function RulesLoader(file_path) {
+	
+	this.file_path = file_path;
+	this.data = null;
+	this.loadRules = function () {
+    	var contents = fs.readFileSync(this.file_path).toString();
+    	// TODO: chequear excepciones
+    	this.data = JSON.parse(contents);
+  	}
+
+  	this.getRules = function () { 
+  		return this.data.rules;
+  	}
+
+  	this.saveSettings = function (settings) {
+    	//TODO: implements
+    	//fs.writeFileSync(this.file_path, JSON.stringify(settings));
+  	}
+}
+
 function main () {
-	console.log("hola mundo");
+	var settings =  {
+		rulesPath: './rules.json'
+	}
+	var rulesLoader = new RulesLoader(settings.rulesPath);
+	var rulesManager = new RuleManager();
+	rulesLoader.loadRules();
+	rulesManager.setRules(rulesLoader.getRules());
+	console.log(rulesManager.getRules())
+
+
+
 }
 
 main();
